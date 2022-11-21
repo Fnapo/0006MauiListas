c4568e92574aad709079268159644b7c6b5d3d56
Fnapo <36490842+Fnapo@users.noreply.github.com>
2022-11-21 17:23:47 +0100

    Revert "Delete Estilos.cs"
    This reverts commit 87e6d4eaaf196549d2b744f78484b8093597c3f1.

---
+++ MisEstilos/Estilos.cs
@@ -0,0 +1,16 @@
+﻿namespace MisEstilos
+{
+	// All the code in this file is included in all platforms.
+	public static class Estilos
+	{
+		private static readonly ResourceDictionary listaEstilos = Application.Current.Resources.MergedDictionaries.ToArray()[2];
+		public static Style EntradaValida { get; } = listaEstilos["EstiloEntryOK"] as Style;
+		public static Style EntradaFalsa { get; } = listaEstilos["EstiloEntryNoOK"] as Style;
+		public static Style AvisoError { get; } = listaEstilos["EstiloLabelAvisoError"] as Style;
+		public static Style AvisoOK { get; } = listaEstilos["EstiloLabelAvisoOK"] as Style;
+		public static Style BotonActivo { get; } = listaEstilos["EstiloBotonActivo"] as Style;
+		public static Style BotonNoActivo { get; } = listaEstilos["EstiloBotonNoActivo"] as Style;
+		public static Style BotonAvanzar { get; } = listaEstilos["EstiloBotonAvanzar"] as Style;
+		public static Style BotonPeligro { get; } = listaEstilos["EstiloBotonPeligro"] as Style;
+	}
+}
